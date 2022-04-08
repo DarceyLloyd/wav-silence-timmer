8d3743cb5e9e870a1782e74a68df468e7b974535
Darcey <Darcey.Lloyd@gmail.com>
2022-04-08 20:42:44 +0100

    1.4.1
    changed to node start

---
+++ trim-old-1.js
@@ -0,0 +1,263 @@
+// Set your target directory
+// NOTE: You need to make sure all the \ in your path are \\ (not sure if this will be an issue on linux or mac but it is on windows \\ not \)
+// let userTargetDir = "W:\\www\\GIT\\wav-silence-timmer\\test_wavs";
+let userTargetDir = "./wavs";
+
+// Adjustments
+// Still getting too much silence on your samples? Play with these settings (lower db threshold values will be tighter but can crop)
+
+
+
+
+
+
+
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+// DONT CHANGE ANYTHING AFTER THIS POINT
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+
+const aftc = require("aftc-node-tools")
+const log = aftc.log;
+const fs = require('fs');
+const fsExtra = require('fs-extra')
+const path = require('path');
+var spawn = require('child_process').spawn;
+let { getFileParams, getFiles, walk, getBitDepth } = require("./libs.js");
+// console.clear();
+
+let ffmpegPath = __dirname + "\\ffmpeg.exe";
+let count = 0;
+let noOfFilesToProcess = 0;
+let errorCount = 0;
+let filesNotProcessed = [];
+
+// Create output folder / or clean it up
+// OutputFolderParams (ofp)
+let ofp = {
+    sourceDir: userTargetDir,
+    correctedSourcePath: false,
+    targetDir: false,
+    correctedTargetPath: false,
+}
+ofp.targetDir = ofp.sourceDir + "\\trimmed";
+// Fix paths to allow for spaces
+ofp.correctedSourcePath = ofp.sourceDir.replace(' ', '\ ');
+ofp.correctedTargetPath = ofp.targetDir.replace(' ', '\ ');
+// log(ofp);
+
+
+let startPeriods = 1;
+let startSilence = 0.01;
+let startThreshold = -60; //dB
+let endPeriods = 1;
+let endSilence = 0.01;
+let endThreshold = -60; //dB
+
+
+
+
+
+
+// Make sure targetPath exists
+if (!fs.existsSync(ofp.targetDir)) {
+    fs.mkdirSync(ofp.targetDir);
+
+    setTimeout(() => {
+        start();
+    }, 500);
+
+} else {
+    // Path exists, remove all file in it!
+
+    fsExtra.emptyDir(ofp.targetDir, err => {
+        if (err) return console.error(err)
+
+        setTimeout(() => {
+            start();
+        }, 500);
+    })
+
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+
+async function start() {
+    await getFiles(userTargetDir)
+        .then(files => {
+            // log(files);
+            noOfFilesToProcess = files.length;
+            log("WAV Silence Trimmer".cyan);
+            log("Supports 16, 24 and 32 bit WAV files only".yellow);
+            log(("Detected: " + noOfFilesToProcess + " files, please wait...").green);
+            log(". . . . . . . . . . . . . . . . . . . . . . . . .".blue);
+
+            let c = 0;
+
+            files.forEach(async (filePath) => {
+                if (!filePath.includes(".wav")) {
+                    errorCount++;
+                    filesNotProcessed.push(filePath);
+                } else {
+                    c++;
+                    let params = getFileParams(filePath);
+                    // log(params);
+
+                    let bitDepth = await getBitDepth(params.correctedSourcePath);
+                    // log(bitDepth);
+
+
+
+                    await trimWav(params)
+                        .then((response) => {
+                            // log(response);
+                            // log(("Processed file: " + c + "/" + noOfFilesToProcess + " - " + params.correctedSourcePath + " [" + params.bitDepth + "Bit]").green);
+                        })
+                        .catch((response) => {
+                            // log("ERROR: " + response);
+                            errorCount++;
+                            // log(("ERROR: " + params.correctedSourcePath + " [" + params.bitDepth + "Bit]").red);
+                        })
+
+                }
+            }); // end files.forEach
+
+        }) // end getFiles.then
+
+    log((errorCount + " files failed...").red);
+    log(filesNotProcessed)
+    log("Done".green);
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+// Snip snip... Come here!
+let trimWav = function (params) {
+    return new Promise((resolve, reject) => {
+
+        // NOTE: 2 Methods
+        // Method 1:
+        // The args commented out below, no reverse and use start and stop vars (works but misses a lot)
+        // Method 2:
+        // Trim one way, reverse, trim again and reverse back
+        // ffmpeg -i "' + src + '" -af silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse "' + (temp + str(c)) + '.wav"'
+
+        // Let's have an argument!
+        // let args = [
+        //     "-loglevel",
+        //     "error",
+        //     "-i",
+        //     params.correctedSourcePath,
+        //     "-af",
+        //     "silenceremove=start_periods=1:start_threshold=-50dB:stop_periods=1:stop_duration=1:stop_threshold=-60dB:",
+        //     // "silenceremove=start_periods=" + startPeriods + ":start_threshold=" + startThreshold + "dB:stop_periods=" + stopPeriods + ":stop_duration=" + stopDuration + ":stop_threshold=" + stopThreshold + "dB:",
+        //     "-c:a",
+        //     "pcm_s32le",
+        //     "-y",
+        //     params.correctedTargetPath
+        // ];
+
+        // ffmpeg -i "' + src + '" -af silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse "' + (temp + str(c)) + '.wav"'
+        let args = false;
+
+
+
+        switch (params.bitDepth) {
+            case 16:
+                args = [
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-i",
+                    params.correctedSourcePath,
+                    "-af",
+                    "silenceremove=start_periods=" + startPeriods + ":start_silence=" + startSilence + ":start_threshold=" + startThreshold + "dB,areverse,silenceremove=start_periods=" + endPeriods + ":start_silence=" + endSilence + ":start_threshold=" + endThreshold + "dB,areverse",
+                    "-c:a",
+                    "pcm_s16le",
+                    "-ac",
+                    "2",
+                    // out
+                    params.correctedTargetPath
+                ]
+                break;
+            case 24:
+                args = [
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-i",
+                    params.correctedSourcePath,
+                    "-af",
+                    "silenceremove=start_periods=" + startPeriods + ":start_silence=" + startSilence + ":start_threshold=" + startThreshold + "dB,areverse,silenceremove=start_periods=" + endPeriods + ":start_silence=" + endSilence + ":start_threshold=" + endThreshold + "dB,areverse",
+                    "-c:a",
+                    "pcm_s24le",
+                    "-ac",
+                    "2",
+                    // out
+                    params.correctedTargetPath
+                ]
+                break;
+            case 32:
+                args = [
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-i",
+                    params.correctedSourcePath,
+                    "-af",
+                    "silenceremove=start_periods=" + startPeriods + ":start_silence=" + startSilence + ":start_threshold=" + startThreshold + "dB,areverse,silenceremove=start_periods=" + endPeriods + ":start_silence=" + endSilence + ":start_threshold=" + endThreshold + "dB,areverse",
+                    "-c:a",
+                    "pcm_s32le",
+                    "-ac",
+                    "2",
+                    // out
+                    params.correctedTargetPath
+                ]
+                break;
+            case 16:
+                args = [
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-i",
+                    params.correctedSourcePath,
+                    "-af",
+                    "silenceremove=start_periods=" + startPeriods + ":start_silence=" + startSilence + ":start_threshold=" + startThreshold + "dB,areverse,silenceremove=start_periods=" + endPeriods + ":start_silence=" + endSilence + ":start_threshold=" + endThreshold + "dB,areverse",
+                    "-c:a",
+                    "pcm_s16le",
+                    "-ac",
+                    "2",
+                    // out
+                    params.correctedTargetPath
+                ]
+                break;
+            default:
+                reject();
+                break;
+        }
+        // log(args.join());
+
+        var proc = spawn(ffmpegPath, args);
+
+        proc.stdout.on('data', function (data) {
+            // console.log(data);
+            // resolve(true);
+        });
+
+        proc.stderr.setEncoding("utf8")
+        proc.stderr.on('data', function (data) {
+            console.log(data);
+            reject();
+        });
+
+        proc.on('close', function () {
+            resolve(true);
+        });
+
+
+
+    })
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
---
+++ trim-old-2.js
@@ -0,0 +1,375 @@
+const aftc = require("aftc-node-tools")
+const log = aftc.log;
+const fs = require('fs');
+const fsExtra = require('fs-extra')
+const path = require('path');
+const utf8 = require('utf8');
+var spawn = require('child_process').spawn;
+let { getFiles } = require("./libs.js");
+// console.clear();
+
+
+
+// TO DO:
+// Trim in real time not all in 1 go (if batch processing 1,000,000 samples it could run out of memory till this is implemented)
+// https://zaiste.net/posts/nodejs-child-process-spawn-exec-fork-async-await/
+
+// Set your target directory
+
+// Want to use the folder in this repo? Use the next line
+let userTargetDir = path.resolve("./wavs");
+
+// Want to use a specific folder on your computer? Use the next line
+// NOTE: Windows paths that use \ must be \\ or use /
+// EG:
+// let userTargetDir = path.resolve("C:\\Users\\darce\\Desktop\\Music Maker\\EDMProd EDM Starter Kit\\1. Samples\\4. FX");
+
+
+
+// Adjustments
+// Still getting too much silence on your samples? Play with these settings (lower db threshold values will be tighter but can crop)
+let startPeriods = 1;
+let startSilence = 0.01;
+let startThreshold = -80; //dB
+let endPeriods = 1;
+let endSilence = 0.01;
+let endThreshold = -80; //dB
+
+
+
+
+
+
+
+
+
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+// DONT CHANGE ANYTHING AFTER THIS POINT
+// # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+let ffmpegPath = __dirname + "\\ffmpeg.exe";
+let ffProbe = __dirname + "\\ffprobe.exe";
+
+let outputDir = path.resolve((userTargetDir + "\\trimmed"));
+
+
+aftc.cls();
+log("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #".blue);
+log(`Creating & Clearing output directory:`.yellow);
+log(`${outputDir}`.green);
+
+
+// Make sure targetPath exists
+if (!fs.existsSync(outputDir)) {
+    fs.mkdirSync(outputDir);
+
+    setTimeout(() => {
+        start(userTargetDir, outputDir);
+    }, 500);
+
+} else {
+    // Path exists, remove all files in it
+    fsExtra.emptyDir(outputDir, err => {
+        if (err) return console.error(err)
+
+        setTimeout(() => {
+            start(userTargetDir, outputDir);
+        }, 500);
+    })
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+
+
+
+async function start(dir, outputDir) {
+
+    let allFiles = [];
+    let files = [];
+    let noOfFiles = 0;
+    let noOfFilesToProcess = 0;
+    let errorCount = 0;
+    let filesNotProcessed = [];
+    let fatalError = false;
+
+
+    log(`Processing samples in:`.yellow);
+    log(`${dir}`.green);
+
+    // Get files to trim
+    allFiles = await getFiles(dir).catch(e => {
+        fatalError = true;
+        log("ERROR GETTING FILES!".red);
+        log(e);
+    })
+    if (fatalError) { reject(false); }
+
+
+    // Process allFiles and only get wav files
+    for await (let f of allFiles) {
+        if (f.includes(".wav")) {
+            files.push(f);
+        }
+    }
+
+    // Vars
+    noOfFiles = files.length;
+    log(`${allFiles.length} files found.`.yellow);
+    log(`${noOfFiles} wav files to trim.`.yellow);
+
+    // log(files);
+
+
+    // Trim
+    let c = 0;
+    files.forEach(async (f) => {
+        c++;
+        // \r clears the line for replacement
+
+        process.stdout.clearLine();
+        process.stdout.write("Trimming " + c + " of " + noOfFiles + " [" + path.basename(f) + "]\r");
+
+        // log(f);
+        let probeData = await getBitDepth(f);
+        let bitRate = false;
+
+        // probeData = probeData.replace(/\s+/, "")
+        probeData = probeData.replace(/(?:\r\n|\r|\n)/g, '-');
+        probeData = probeData.replaceAll(" ", "");
+        // probeData = probeData.replaceAll("\n","");
+        // log(probeData);
+
+        let temp1 = probeData.indexOf(`"bits_per_sample":`);
+        let temp2 = probeData.indexOf(`,-"r_frame_rate"`);
+
+        // log(temp1);
+        // log(temp2);
+        // let temp3 = probeData.substring(temp1,0);
+        let temp3 = probeData.substr((temp1), 30);
+        temp3 = temp3.replaceAll(`,-"r_frame`, "");
+        temp3 = temp3.replaceAll(`"bits_per_sample":`, "");
+
+        // let temp3 = probeData.split(`bits_per_sample`,10);
+        // log(temp3);
+        bitRate = parseInt(temp3);
+
+
+
+        await trimWav(f, outputDir, bitRate).catch(e => {
+            error = true;
+            errorCount++;
+            filesNotProcessed.push(f);
+        })
+
+
+
+    });
+
+    // End response
+    process.stdout.clearLine();
+    if (errorCount > 0) {
+        log((errorCount + " files failed to be trimmed...").red);
+        // log(filesNotProcessed)
+        filesNotProcessed.forEach(f => {
+            log(f + " - FAILED!".red);
+        });
+    }
+    log("Done".green);
+    log("# # # # # # # # # # # # # # # # # # # # # # # # # # # # # #".blue);
+
+}
+
+
+
+
+async function getBitDepth(f) {
+    return new Promise((resolve, reject) => {
+        // let args = [
+        //     "-loglevel",
+        //     "panic",
+        //     "-show_format",
+        //     "stream=bits_per_raw_sample",
+        //     "-select_streams",
+        //     "-of",
+        //     "json",
+        //     "v",
+        //     f
+        // ]
+
+        // let args2 = [
+        //     "-hide_banner",
+        //     "-loglevel",
+        //     "panic",
+        //     "-show_format",
+        //     "-show_streams",
+        //     "-of",
+        //     "json",
+        //     f
+        // ]
+
+        let args3 = [
+            "-loglevel",
+            "panic",
+            "-show_streams",
+            "-of",
+            "json",
+            f
+        ]
+
+        let proc = spawn(ffProbe, args3);
+
+        proc.stdout.setEncoding("utf8")
+        proc.stdout.on('data', function (data) {
+            // Data contains the information we need
+            // log(data);
+            // log(`${typeof(data)}`.red)
+            // log(data);
+            // data = utf8.encode(data)
+            // let j = JSON.parse(data);
+            // let bitRate = parseInt(j.streams[0].bits_per_raw_sample);
+            // resolve(bitRate);
+            resolve(data);
+        });
+
+        proc.stderr.setEncoding("utf8")
+        proc.stderr.on('data', function (data) {
+            // log(`ERROR`.red);
+            reject(false);
+        });
+
+        proc.on('close', function (data) {
+            // log(f);
+            // log(data);
+            // resolve(true);
+            // return true;
+        });
+
+    });
+
+}
+
+
+
+
+
+
+// Trim time
+async function trimWav (source, output, bitDepth) {
+    return new Promise((resolve, reject) => {
+
+        // NOTE: 2 Methods
+        // Method 1:
+        // The args commented out below, no reverse and use start and stop vars (works but misses a lot)
+        // Method 2:
+        // Trim one way, reverse, trim again and reverse back
+        // ffmpeg -i "' + src + '" -af silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse "' + (temp + str(c)) + '.wav"'
+
+        // Let's have an argument!
+        // let args = [
+        //     "-loglevel",
+        //     "error",
+        //     "-i",
+        //     params.correctedSourcePath,
+        //     "-af",
+        //     "silenceremove=start_periods=1:start_threshold=-50dB:stop_periods=1:stop_duration=1:stop_threshold=-60dB:",
+        //     // "silenceremove=start_periods=" + startPeriods + ":start_threshold=" + startThreshold + "dB:stop_periods=" + stopPeriods + ":stop_duration=" + stopDuration + ":stop_threshold=" + stopThreshold + "dB:",
+        //     "-c:a",
+        //     "pcm_s32le",
+        //     "-y",
+        //     params.correctedTargetPath
+        // ];
+
+        // ffmpeg -i "' + src + '" -af silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse,silenceremove=start_periods=1:start_silence=0.1:start_threshold=-50dB,areverse "' + (temp + str(c)) + '.wav"'
+        let args = false;
+
+
+        let outputFile = path.resolve((output + "/" + path.basename(source)));
+
+
+        switch (bitDepth) {
+            case 16:
+                args = [
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-i",
+                    source,
+                    "-af",
+                    "silenceremove=start_periods=" + startPeriods + ":start_silence=" + startSilence + ":start_threshold=" + startThreshold + "dB,areverse,silenceremove=start_periods=" + endPeriods + ":start_silence=" + endSilence + ":start_threshold=" + endThreshold + "dB,areverse",
+                    "-c:a",
+                    "pcm_s16le",
+                    "-ac",
+                    "2",
+                    // out
+                    outputFile
+                ]
+                break;
+            case 24:
+                args = [
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-i",
+                    source,
+                    "-af",
+                    "silenceremove=start_periods=" + startPeriods + ":start_silence=" + startSilence + ":start_threshold=" + startThreshold + "dB,areverse,silenceremove=start_periods=" + endPeriods + ":start_silence=" + endSilence + ":start_threshold=" + endThreshold + "dB,areverse",
+                    "-c:a",
+                    "pcm_s24le",
+                    "-ac",
+                    "2",
+                    // out
+                    outputFile
+                ]
+                break;
+            case 32:
+                args = [
+                    "-hide_banner",
+                    "-loglevel",
+                    "error",
+                    "-i",
+                    source,
+                    "-af",
+                    "silenceremove=start_periods=" + startPeriods + ":start_silence=" + startSilence + ":start_threshold=" + startThreshold + "dB,areverse,silenceremove=start_periods=" + endPeriods + ":start_silence=" + endSilence + ":start_threshold=" + endThreshold + "dB,areverse",
+                    "-c:a",
+                    "pcm_s32le",
+                    "-ac",
+                    "2",
+                    // out
+                    outputFile
+                ]
+                break;
+
+            default:
+                log(`ERROR ON ${source} - bitDepth = ${bitDepth}`.red);
+                reject();
+                break;
+        }
+        // log(args.join());
+
+
+
+
+        var proc = spawn(ffmpegPath, args);
+
+        proc.stdout.on('data', function (data) {
+            // console.log(data);
+            // resolve(true);
+        });
+
+        proc.stderr.setEncoding("utf8")
+        proc.stderr.on('data', function (data) {
+            // console.log(data);
+            log("ERROR".red);
+            console.error(data);
+            reject(false);
+        });
+
+        proc.on('close', function () {
+            resolve(true);
+        });
+
+
+
+    })
+}
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
